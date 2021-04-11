4cf9c920b9186612013a437e28afedb8aed58af2
Liam Cole <liam@liamiam.com>
2021-04-11 14:53:06 -0700

    silence

--- Test/Program.cs
+++ Test/Program.cs
@@ -6,48 +6,12 @@ using Castaway.Level;
 using Castaway.Render;
 using static Castaway.Render.VertexAttribInfo.AttribValue;
 
-[ControllerInfo(Name = "Test Controller", Icon = "/test.controller.png")]
-public class TestController : Controller
-{
-    public override void OnBegin()
-    {
-        base.OnBegin();
-        Console.WriteLine("OnBegin");
-    }
-
-    public override void OnEnd()
-    {
-        base.OnEnd();
-        Console.WriteLine("OnEnd");
-    }
-
-    public override void OnDraw()
-    {
-        base.OnDraw();
-        Console.WriteLine("OnDraw");
-    }
-
-    public override void OnUpdate()
-    {
-        base.OnUpdate();
-        Console.WriteLine("OnUpdate");
-    }
-}
-
 internal static class Program
 {
     private static ShaderHandle _shaderHandle;
     
     private static void Main(string[] args)
     {
-        var level = new Level();
-        var @ref = level.Create();
-        
-        var o = @ref.Object;
-        o.Add<TestController>();
-        @ref.Object = o;
-        
-        Events.PostInit += level.Activate;
         Events.Init += () =>
         {
             _shaderHandle = AssetManager.Get<LoadedShader>("/test.shdr")?.ToHandle();
@@ -65,7 +29,6 @@ internal static class Program
             vbo.Add(1, 0, r: 0, g: 0, b: 1);
             vbo.Draw();
         };
-        Events.CloseNormally += level.Deactivate;
         Cast.Start();
     }
 }
