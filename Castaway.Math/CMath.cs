952bab70a93db4978d0562ce96e2840a32bb37c2
Liam Cole <liam@liamiam.com>
2021-04-17 13:35:57 -0700

    add lerp

--- Castaway.Math/CMath.cs
+++ Castaway.Math/CMath.cs
@@ -6,5 +6,6 @@ namespace Castaway.Math
     {
         public static float Radians(float degrees) => degrees * (MathF.PI / 180f);
         public static float Degrees(float radians) => radians * (180f / MathF.PI);
+        public static float Lerp(float a, float b, float i) => a + (b - a) * i;
     }
 }
