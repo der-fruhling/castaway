02d04eb07e4eaec4e95e6c81850eab1f1bf02ef3
Liam Cole <liam@liamiam.com>
2021-06-06 18:15:57 -0700

    Invert some if() statements

--- Castaway/Castaway.cs
+++ Castaway/Castaway.cs
@@ -11,14 +11,10 @@ namespace Castaway
             var doc = JsonDocument.Parse(File.ReadAllText("config.json"));
             var root = doc.RootElement;
             AssetLoader.Loader = new AssetLoader();
-            if (root.TryGetProperty("assets", out var assetsElement))
-            {
-                if (assetsElement.TryGetProperty("discover", out var discoverElement))
-                {
-                    foreach(var s in discoverElement.EnumerateArray())
-                        AssetLoader.Loader.Discover(s.GetString()!);
-                }
-            }
+            if (!root.TryGetProperty("assets", out var assetsElement)) return;
+            if (!assetsElement.TryGetProperty("discover", out var discoverElement)) return;
+            foreach(var s in discoverElement.EnumerateArray())
+                AssetLoader.Loader.Discover(s.GetString()!);
         }
     }
 }
