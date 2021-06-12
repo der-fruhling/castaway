0deb8da1057242eedb13a2cac7a3bcd1524a8112
Liam Cole <liam@liamiam.com>
2021-06-12 01:16:09 -0700

    Example better

--- Test/Program.cs
+++ Test/Program.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using Castaway;
+﻿using Castaway;
 using Castaway.Level;
-using Castaway.Math;
 using Castaway.OpenGL;
-using Castaway.OpenGL.Input;
-using GLFW;
 using static Castaway.Assets.AssetLoader;
 
 namespace Test
@@ -39,41 +35,6 @@ namespace Test
                 level.Render();
                 g.FinishFrame(window);
                 level.Update();
-                
-                // MOVEMENT CODE
-                var obj = level.Get("Camera");
-                const float speed = 0.125f;
-                const float rotateSpeed = 1f;
-                var move = new Vector3(0, 0, 0);
-                var rotate = new Quaternion(1, 0, 0, 0);
-                
-                // Gamepad
-                if (InputSystem.Gamepad.Valid)
-                {
-                    var moveGamepad = InputSystem.Gamepad.LeftStick * speed;
-                    move.X += moveGamepad.X;
-                    move.Y -= moveGamepad.Y;
-                    move.Z += (InputSystem.Gamepad.RightTrigger - InputSystem.Gamepad.LeftTrigger) * speed;
-
-                    var rotateGamepad = -InputSystem.Gamepad.RightStick * rotateSpeed;
-                    rotate *= Quaternion.Rotation(
-                        0,
-                        MathEx.ToRadians(rotateGamepad.X),
-                        MathEx.ToRadians(rotateGamepad.Y));
-                }
-                
-                // Keyboard
-                if (InputSystem.Keyboard.IsDown(Keys.A)) move.X -= speed;
-                if (InputSystem.Keyboard.IsDown(Keys.D)) move.X += speed;
-                if (InputSystem.Keyboard.IsDown(Keys.S)) move.Y -= speed;
-                if (InputSystem.Keyboard.IsDown(Keys.W)) move.Y += speed;
-                if (InputSystem.Keyboard.IsDown(Keys.Q)) move.Z -= speed;
-                if (InputSystem.Keyboard.IsDown(Keys.E)) move.Z += speed;
-
-                obj.Position += move;
-                obj.Rotation *= rotate;
-                
-                Console.WriteLine(obj.Rotation.ToEulerAngles());
             }
             
             level.End();
