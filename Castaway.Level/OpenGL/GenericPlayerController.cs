e28ef9fdfa7e72d00d253266ff98f91c14936bc1
Liam Cole <liam@liamiam.com>
2021-06-12 01:23:54 -0700

    Update basic controller with options

--- Castaway.Level/OpenGL/GenericPlayerController.cs
+++ Castaway.Level/OpenGL/GenericPlayerController.cs
@@ -8,30 +8,56 @@ namespace Castaway.Level.OpenGL
     {
         private float _rx, _ry;
         
+        [LevelSerialized("MovementSpeed")] public float MovementSpeed = 0.125f;
+        [LevelSerialized("RotationSpeed")] public float RotationSpeed = 2f;
+        [LevelSerialized("Lock.Depth")] public bool DepthLocked = false;
+        [LevelSerialized("Lock.Rotation")] public bool RotationLocked = false;
+        [LevelSerialized("Lock.Movement")] public bool MovementLocked = false;
+        [LevelSerialized("Lock.X")] public bool MovementXLocked = false;
+        [LevelSerialized("Lock.Y")] public bool MovementYLocked = false;
+        [LevelSerialized("Lock.Z")] public bool MovementZLocked = false;
+        
         public override void OnUpdate(LevelObject parent)
         {
             base.OnUpdate(parent);
-            const float speed = 0.125f;
-            var rotateSpeed = MathEx.ToRadians(2);
+            var rotateSpeed = MathEx.ToRadians(RotationSpeed);
             var move = new Vector3(0, 0, 0);
             
             // Gamepad
             if (InputSystem.Gamepad.Valid)
             {
-                var moveGamepad = InputSystem.Gamepad.LeftStick * speed;
-                move.X += moveGamepad.X;
-                move.Z += moveGamepad.Y;
+                if (!MovementLocked)
+                {
+                    var moveGamepad = InputSystem.Gamepad.LeftStick * MovementSpeed;
+                    if(!MovementXLocked) move.X += moveGamepad.X;
+                    if(DepthLocked && !MovementYLocked) move.Y += moveGamepad.Y;
+                    else if(!MovementZLocked) move.Z += moveGamepad.Y;
+                }
 
-                var rotateGamepad = -InputSystem.Gamepad.RightStick * rotateSpeed;
-                _rx += rotateGamepad.X;
-                _ry += rotateGamepad.Y;
+                if (!RotationLocked)
+                {
+                    var rotateGamepad = -InputSystem.Gamepad.RightStick * rotateSpeed;
+                    _rx += rotateGamepad.X;
+                    _ry += rotateGamepad.Y;
+                }
             }
-            
+
             // Keyboard
-            if (InputSystem.Keyboard.IsDown(Keys.A)) move.X -= speed;
-            if (InputSystem.Keyboard.IsDown(Keys.D)) move.X += speed;
-            if (InputSystem.Keyboard.IsDown(Keys.W)) move.Z -= speed;
-            if (InputSystem.Keyboard.IsDown(Keys.S)) move.Z += speed;
+            if (!MovementLocked)
+            {
+                if (InputSystem.Keyboard.IsDown(Keys.A)) move.X -= MovementSpeed;
+                if (InputSystem.Keyboard.IsDown(Keys.D)) move.X += MovementSpeed;
+                if (InputSystem.Keyboard.IsDown(Keys.W)) move.Z -= MovementSpeed;
+                if (InputSystem.Keyboard.IsDown(Keys.S)) move.Z += MovementSpeed;
+            }
+
+            if (!RotationLocked)
+            {
+                if (InputSystem.Keyboard.IsDown(Keys.Up)) _ry += rotateSpeed;
+                if (InputSystem.Keyboard.IsDown(Keys.Down)) _ry -= rotateSpeed;
+                if (InputSystem.Keyboard.IsDown(Keys.Left)) _rx += rotateSpeed;
+                if (InputSystem.Keyboard.IsDown(Keys.Right)) _rx -= rotateSpeed;
+            }
 
             var rotate = new Quaternion(1, 0, 0, 0);
             rotate *= Quaternion.Rotation(Vector3.Up, _rx);
