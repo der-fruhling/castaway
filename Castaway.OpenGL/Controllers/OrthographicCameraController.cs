b2eb5e280a3c0aa3cff89699094dedb644927251
Liam Cole <liam@liamiam.com>
2021-06-24 00:05:15 -0700

    Optimise Camera Controllers

--- Castaway.OpenGL/Controllers/OrthographicCameraController.cs
+++ Castaway.OpenGL/Controllers/OrthographicCameraController.cs
@@ -7,11 +7,32 @@ namespace Castaway.OpenGL.Controllers
     [ControllerName("OrthoCamera")]
     public class OrthographicCameraController : CameraController
     {
+
+        public override void OnInit(LevelObject parent)
+        {
+            base.OnInit(parent);
+            var win = Graphics.Current.Window!;
+            win.WindowResize += OnWindowResize;
+            win.GetSize(out var w, out var h);
+            OnWindowResize(w, h);
+        }
+
+        public override void OnDestroy(LevelObject parent)
+        {
+            base.OnDestroy(parent);
+            Graphics.Current.Window!.WindowResize -= OnWindowResize;
+        }
+
+        private void OnWindowResize(int w, int h)
+        {
+            var win = Graphics.Current.Window!;
+            PerspectiveTransform = CameraMath.Ortho(win, FarCutoff, NearCutoff, Size);
+        }
+        
         public override void PreRenderFrame(LevelObject camera, LevelObject? parent)
         {
             base.PreRenderFrame(camera, parent);
             var w = Graphics.Current.Window!;
-            PerspectiveTransform = CameraMath.Ortho(w, FarCutoff, NearCutoff, Size);
             ViewTransform = Matrix4.Translate(-camera.RealPosition);
         }
     }
--- Castaway.OpenGL/Controllers/PerspectiveCameraController.cs
+++ Castaway.OpenGL/Controllers/PerspectiveCameraController.cs
@@ -8,12 +8,31 @@ namespace Castaway.OpenGL.Controllers
     public class PerspectiveCameraController : CameraController
     {
         [LevelSerialized("VerticalFOV")] public float FOV;
-        
+
+        public override void OnInit(LevelObject parent)
+        {
+            base.OnInit(parent);
+            var win = Graphics.Current.Window!;
+            win.WindowResize += OnWindowResize;
+            win.GetSize(out var w, out var h);
+            OnWindowResize(w, h);
+        }
+
+        public override void OnDestroy(LevelObject parent)
+        {
+            base.OnDestroy(parent);
+            Graphics.Current.Window!.WindowResize -= OnWindowResize;
+        }
+
+        private void OnWindowResize(int w, int h)
+        {
+            var win = Graphics.Current.Window!;
+            PerspectiveTransform = CameraMath.Persp(win, FarCutoff, NearCutoff, MathEx.ToRadians(FOV), Size);
+        }
+
         public override void PreRenderFrame(LevelObject camera, LevelObject? parent)
         {
             base.PreRenderFrame(camera, parent);
-            var w = Graphics.Current.Window!;
-            PerspectiveTransform = CameraMath.Persp(w, FarCutoff, NearCutoff, MathEx.ToRadians(FOV), Size);
             ViewTransform = camera.Rotation.Normalize().Conjugate().ToMatrix4()
                             * Matrix4.Translate(-camera.RealPosition);
         }
