05935d45caa000de320d68a7c3d94b40787feb1d
Liam Cole <liam@liamiam.com>
2021-06-30 20:17:07 -0700

    more logging

--- Castaway.Base/CastawayGlobal.cs
+++ Castaway.Base/CastawayGlobal.cs
@@ -3,13 +3,13 @@ using System.Diagnostics;
 using System.IO;
 using System.Text.Json;
 using System.Threading;
-using System.Threading.Tasks;
 using Serilog;
 using Serilog.Core;
 using Serilog.Enrichers;
 using Serilog.Events;
 using Serilog.Exceptions.Core;
 using Serilog.Formatting.Compact;
+using Serilog.Formatting.Display;
 
 namespace Castaway.Base
 {
@@ -18,52 +18,63 @@ namespace Castaway.Base
         public static readonly LoggingLevelSwitch LevelSwitch = new(LogEventLevel.Debug);
 
         private static volatile object _lock = new();
-        private static volatile bool _ok = true;
+        private static volatile bool _ok;
         private static volatile bool _continue = true;
+
+        private static LogEventLevel _logLevel = LogEventLevel.Information;
+
+        private static string _consoleLogTemplate =
+            "[{Level:u3} @ {Timestamp:MM/dd/yyyy HH:mm:ss.ffffff}; {SourceContext} | {ThreadName}({ThreadId})]: {Message:lj}{NewLine}{Exception}";
+
         public static double FrameTime { get; private set; } = 1.0 / 60.0;
         public static double Framerate => 1 / FrameTime;
         public static double RealFrameTime { get; private set; }
 
+        [Obsolete("Weird; use " + nameof(GetLogger) + "() instead")]
         public static ILogger GetLogger(Type type)
         {
             return Log.Logger.ForContext(type);
         }
 
+        /// <summary>
+        /// Creates a logger based on the calling type.
+        /// </summary>
+        /// <returns>The logger that was created.</returns>
         public static ILogger GetLogger()
         {
-            return GetLogger(new StackTrace().GetFrame(1)!.GetMethod()!.DeclaringType!);
+            return Log.Logger.ForContext(new StackTrace().GetFrame(1)!.GetMethod()!.DeclaringType!);
         }
 
+        /// <summary>
+        /// Runs the specified application. Should be called on the main
+        /// thread.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
         public static int Run<T>() where T : class, IApplication, new()
         {
+            LoadConfig();
             Log.Logger = new LoggerConfiguration()
                 .Enrich.With(new ThreadNameEnricher(), new ExceptionEnricher(), new ThreadIdEnricher())
                 .WriteTo.Console(
-                    outputTemplate:
-                    "[{Level:u3} @ {Timestamp:HH:mm:ss.ffffff}; {SourceContext}]: {Message:lj}{NewLine}{Exception}")
-                .WriteTo.File("castaway.log",
-                    outputTemplate:
-                    "{Level:w} {Timestamp:HH:mm:ss.ffffff} [{ThreadName}|{ThreadId}] : {SourceContext}; {Message:lj}{NewLine}{Exception}")
-                .WriteTo.File(new CompactJsonFormatter(), "castaway.log.jsonl", LogEventLevel.Debug)
+                    outputTemplate: _consoleLogTemplate,
+                    restrictedToMinimumLevel: _logLevel,
+                    standardErrorFromLevel: LogEventLevel.Error)
+                .WriteTo.File(new MessageTemplateTextFormatter(
+                        "{Level:w} {Timestamp:HH:mm:ss.ffffff} [{ThreadName}|{ThreadId}] : {SourceContext}; {Message:lj}{NewLine}{Exception}"),
+                    "logs/castaway-.log",
+                    rollingInterval: RollingInterval.Hour)
+                .WriteTo.File(new CompactJsonFormatter(), "logs/castaway-.jsonl", rollingInterval: RollingInterval.Hour)
                 .MinimumLevel.ControlledBy(LevelSwitch)
                 .CreateLogger();
             Thread.CurrentThread.Name = "MainThread";
             var logger = GetLogger();
 
-            logger.Verbose("a");
-            logger.Debug("b");
-            logger.Information("c");
-            logger.Warning("d");
-            logger.Error("e");
-            logger.Fatal("f");
-
-            logger.Information("Hello");
+            logger.Information("Starting at {StartDateTime}", DateTime.Now);
 
             var returnCode = 0;
             var timeKill = new Thread(TimeKill) {Name = "TimeKill"};
             timeKill.Start();
-
-            LoadConfig(logger);
 
             var application = new T();
             application.Init();
@@ -115,26 +126,18 @@ namespace Castaway.Base
             return returnCode;
         }
 
-        private static void LoadConfig(ILogger logger)
+        private static void LoadConfig()
         {
             using var json = JsonDocument.Parse(File.ReadAllText("config.json"));
             var root = json.RootElement;
             var eLog = root.GetProperty("log");
 
-            Try(logger, delegate
-            {
-                var eLogLevelS = eLog.OptionalProperty("level")?.GetString()
-                                 ?? Enum.GetName(LogEventLevel.Information)!;
-                var eLogLevel = TryParseEnum<LogEventLevel>(eLogLevelS, true);
-                LevelSwitch.MinimumLevel = eLogLevel
-                                           ?? throw new EnumParseException(
-                                               "/log/level", eLogLevelS, typeof(LogEventLevel),
-                                               () => LevelSwitch.MinimumLevel = LogEventLevel.Information,
-                                               LogEventLevel.Information);
-                logger.Verbose("LogLevel = {Value}", eLogLevel);
-            });
+            if (eLog.TryGetProperty("level", out var e))
+                _logLevel = TryParseEnum<LogEventLevel>(e.GetString()!, true)
+                            ?? throw new InvalidOperationException($"Invalid log level: {e.GetString()}");
 
-            logger.Debug("Loaded config from config.json");
+            if (eLog.TryGetProperty("template", out e))
+                _consoleLogTemplate = e.GetString()!;
         }
 
         private static JsonElement? OptionalProperty(this JsonElement e, string name)
@@ -145,30 +148,6 @@ namespace Castaway.Base
         private static T? TryParseEnum<T>(string name, bool ignoreCase = false) where T : struct
         {
             return Enum.TryParse<T>(name, ignoreCase, out var t) ? t : null;
-        }
-
-        private static void Try(ILogger logger, Action a)
-        {
-            try
-            {
-                a();
-            }
-            catch (MessageException e)
-            {
-                if (e.IsFatal) throw;
-                e.Log(logger);
-                e.Repair(logger);
-            }
-            catch (Exception e)
-            {
-                logger.Error("{Type} occurred in config stage: /log/level", e.GetType());
-                throw;
-            }
-        }
-
-        private static Task TryAsync(ILogger logger, Action a)
-        {
-            return Task.Run(() => Try(logger, a));
         }
 
         private static void TimeKill()
