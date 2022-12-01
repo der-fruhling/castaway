8d576fed36d2ec397dac7897f606b83d3dff5de5
liam <liam_lightchild@outlook.com>
2022-12-01 15:49:15 -0800

    Use some features of OpenGL 4.5
    Signed-off-by: liam <liam_lightchild@outlook.com>

---
+++ Castaway.OpenGL/OpenGL43.cs
@@ -0,0 +1,9 @@
+using Castaway.Rendering;
+
+namespace Castaway.OpenGL;
+
+[Implements("OpenGL-4.3")]
+public class OpenGL43 : OpenGL42
+{
+	public override string Name => "OpenGL-4.3";
+}
---
+++ Castaway.OpenGL/OpenGL44.cs
@@ -0,0 +1,9 @@
+using Castaway.Rendering;
+
+namespace Castaway.OpenGL;
+
+[Implements("OpenGL-4.4")]
+public class OpenGL44 : OpenGL43
+{
+	public override string Name => "OpenGL-4.4";
+}
